537b5ba52711b9359bf6d5ba8988403a1af5547d
Eric Cornelissen <ericornelissen@gmail.com>
2018-01-20 14:59:28 +0100

    Detect branch and new files, commit file to seperate branch

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
 module.exports = (robot) => {
-  // Your code here
   console.log('Yay, the app was loaded!')
 
-  // For more information on building apps:
-  // https://probot.github.io/docs/
+  robot.on('push', async context => {
+    let push = context.payload;
 
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
+    //
+    let branch = push.ref.replace('refs/heads/', '');
+    if (branch !== process.env.LISTEN_BRANCH) {
+      robot.log.info({base: push.ref}, 'event ignored, branch not of interest')
+      return
+    }
+
+    push.commits.forEach(commit => {
+      let added = [];
+      commit.added.filter(file => !added.includes(file))
+        .forEach(async file => {
+          added.push(file);
+
+          //
+          robot.log.info({file: file}, '1 new file detected')
+
+          //
+          let commit = context.repo({
+            branch: 'test-bot',
+            content: new Buffer("Hello World").toString('base64'),
+            message: `Add ${file} as pdf`,
+            path: file.replace('.svg', '.pdf')
+          });
+
+          //
+          context.github.repos.createFile(commit);
+        })
+    })
+  })
 }
